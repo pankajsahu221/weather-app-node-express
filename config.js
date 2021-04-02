22ba28dc961eb07489cb1633a3f73e0b6d38e229
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2021-04-02 19:13:21 +0000

    Hidden key

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "f60962b78d034f1bcf6078f4c5c92d13"
+        SECRET_KEY: "YOUR_KEY"
     }
 }
 
